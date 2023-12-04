1b38301538db8d40cb2be1ed5ae60f30910c2c1e
kauandp <136329605+kauandp@users.noreply.github.com>
2023-12-04 08:55:45 -0300

    Add files via upload

---
+++ 2/Java-leu-main/main.js
@@ -0,0 +1,17 @@
+//Pom
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom') .play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+
+//enquanto
+while (contador < ListaDeTeclas.length) {
+    ListaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = (contador + 1); 
+    console.log(contador);
+}
